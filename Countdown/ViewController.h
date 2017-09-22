fc575eeb2df64d67c1177121fd4e0890b2ffebfe
Kanchan <ikan.verma@gmail.com>
2017-09-22 15:10:30 +0530

    changed

--- Countdown/ViewController.h
+++ Countdown/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *remainTimeLbl;
+@property (weak, nonatomic) IBOutlet UIView *timerFrameVW;
 
 @end
 
